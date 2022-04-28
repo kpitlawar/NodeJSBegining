ce0f696d0bfb9cb6de30ee1709f7159a815d48a5
Kanchan Pitlawar <kanchan@avegenhealth.com>
2022-04-28 17:17:38 +0530

    basic routing

--- server.js
+++ server.js
@@ -6,8 +6,29 @@ const server = http.createServer((req, res) =>{
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    // routing
+    let path = './views/';
+  switch(req.url) {
+    case '/':
+      path += 'index.html';
+      res.statusCode = 200;
+      break;
+    case '/about':
+      path += 'about.html';
+      res.statusCode = 200;
+      break;
+    case '/about-us':
+      res.statusCode = 301;
+      res.setHeader('Location', '/about');
+      res.end();
+      break;
+    default:
+      path += '404.html';
+      res.statusCode = 404;
+  }
+
     // send an html file
-    fs.readFile('./views/index.html', (error, data)=>{
+    fs.readFile(path, (error, data)=>{
         if(error){
             console.log(error);
             res.end();
@@ -17,6 +38,7 @@ const server = http.createServer((req, res) =>{
         }
 
     })
+    
    
 });
 
