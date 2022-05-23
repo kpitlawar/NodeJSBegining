9c4cf48aa0d57b3b8b37c1a9dd5ad648e7bd290b
Kanchan Pitlawar <kanchan@avegenhealth.com>
2022-05-23 18:25:45 +0530

    partial tempalte using ejs footer|nav|header

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
 const express = require('express');
+const morgan = require('morgan')
 
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 app.listen(3000);
+
+// static file middleware
+app.use(express.static('public'))
+
+app.use(morgan('dev'));
+
+// Logger middleware
+// app.use((req, res, next)=>{
+//     console.log('new request made:');
+//     console.log('host:',req.hostname);
+//     console.log('path:',req.path);
+//     console.log('method:',req.method);
+//     next();
+
+// })
 
 app.get('/', (req, res)=>{
     // res.send('<p> Home page</p>') // sends header automatically along with statusCode
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', {title:'Home', blogs})
 });
 
 app.get('/about', (req, res)=>{
     // res.send('<p> About page</p>') // sends header automatically along with statusCode
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about',  {title:'About'})
 });
 
-//redirect
-
-app.get('/about-me',(req, res)=>{
-    res.redirect('/about')
-});
+app.get('/blogs/create', (req, res)=>{
+    res.render('create', {title:'Create a new blog'});
+})
 
 // 404 page
 // use is used to create middleware and fire middleware function in express
 // use will fire for every single request
 // position of use function should be at the end for route
+// example of middleware
 
 app.use((req, res)=>{
-    res.sendFile('./views/404.html', {root: __dirname})
+    res.status(404).render('404', {title:'404'});
 
 })
